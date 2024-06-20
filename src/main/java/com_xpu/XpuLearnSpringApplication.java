b16f60c5c96605480ae610c9f77ff728edf3d165
kanghuarui <kanghuarui2024@163.com>
2024-06-20 15:21:05 +0800

    新建的学习项目-xpu校园交友

---
+++ src/main/java/com_xpu/XpuLearnSpringApplication.java
@@ -0,0 +1,13 @@
+package com_xpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpuLearnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpuLearnSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com_xpu/XpuLearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com_xpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpuLearnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
